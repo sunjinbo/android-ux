b86b606a5d2881eaaab27bbd9ec03899c9d800ff
sunjinbo <sunjinbo@360.com>
2019-01-21 16:15:58 +0800

    create basic app framework

---
+++ app/src/main/java/com/android/ux/ux/AsynchronousActivity.java
@@ -0,0 +1,13 @@
+package com.android.ux.ux;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AsynchronousActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asynchronous);
+    }
+}
