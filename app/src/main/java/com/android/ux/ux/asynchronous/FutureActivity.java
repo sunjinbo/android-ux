af2a26f9a01d7bc96b2fa010994bbdf33c8f944c
sunjinbo <sunjinbo@360.com>
2019-02-13 16:33:52 +0800

    runnable+callable

--- app/src/main/java/com/android/ux/ux/asynchronous/FutureActivity.java
+++ app/src/main/java/com/android/ux/ux/asynchronous/FutureActivity.java
@@ -1,15 +1,61 @@
 package com.android.ux.ux.asynchronous;
 
-import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.SystemClock;
 
-import com.android.ux.ux.R;
+import com.android.ux.ux.BaseActivity;
 
-public class FutureActivity extends Activity {
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+public class FutureActivity extends BaseActivity implements Runnable, Callable<String> {
+
+    private boolean mIsDestroy = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_future);
+
+        final Thread r = new Thread(this);
+        r.start();
+
+        final FutureTask<String> task = new FutureTask<>(this);
+        final Thread thread = new Thread(task);
+        thread.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mIsDestroy = true;
+    }
+
+    @Override
+    public void run() {
+        while (!mIsDestroy) {
+            SystemClock.sleep(666);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    addMessage("Runnable is ticked!", Color.GREEN);
+                }
+            });
+        }
+    }
+
+    @Override
+    public String call() throws Exception {
+        while (!mIsDestroy) {
+            SystemClock.sleep(666);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    addMessage("Callable is ticked!", Color.BLUE);
+                }
+            });
+        }
+
+        return null;
     }
 }
