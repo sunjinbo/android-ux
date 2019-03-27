692707282ff6a18f6c0c434cbd4da00b18732fbe
sunjinbo <sunjinbo@360.com>
2019-03-27 18:02:39 +0800

    add ThreadLocal&Lock&AIDL testing

---
+++ app/src/main/java/com/android/ux/ux/asynchronous/ThreadLocalActivity.java
@@ -0,0 +1,15 @@
+package com.android.ux.ux.asynchronous;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.android.ux.ux.R;
+
+public class ThreadLocalActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thread_local);
+    }
+}
