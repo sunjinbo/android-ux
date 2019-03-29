5522d86205f2f11daced2874ded5f82e6f533be5
sunjinbo <sunjinbo@360.com>
2019-03-29 17:32:36 +0800

    implements two animation custom views

--- app/src/main/java/com/android/ux/ux/asynchronous/AIDLActivity.java
+++ app/src/main/java/com/android/ux/ux/asynchronous/AIDLActivity.java
@@ -11,5 +11,7 @@ public class AIDLActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aidl);
+
+
     }
 }
---
+++ app/src/main/java/com/android/ux/ux/layout/SquareImageView.java
@@ -0,0 +1,26 @@
+package com.android.ux.ux.layout;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class SquareImageView extends ImageView {
+    public SquareImageView(@NonNull Context context) {
+        super(context);
+    }
+
+    public SquareImageView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
