8b42427655bc47131c9f5d96ffbe933ecd2401bf
sunjinbo <sunjinbo@360.com>
2019-10-28 18:42:21 +0800

    add network status monitoriing logic

---
+++ app/src/main/java/com/android/ux/ux/broadcast/IncomingCallActivity.java
@@ -0,0 +1,12 @@
+package com.android.ux.ux.broadcast;
+
+import android.os.Bundle;
+
+import com.android.ux.ux.BaseActivity;
+
+public class IncomingCallActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
